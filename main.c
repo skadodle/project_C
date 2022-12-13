e3dfb59b6972d7cb9978f9632f4f161c6dd2c9fb
Afanasev Aleksandr <afanasev.aleksandrlm@gmail.com>
2022-12-13 15:20:32 +0300

    Alpha version

--- main.c
+++ main.c
@@ -8,6 +8,14 @@ typedef struct {
     float percent; 
 } np;
 
+int compare(const void *a, const void *b) {
+  
+    np *npA = (np *)a;
+    np *npB = (np *)b;
+  
+    return (npB->percent - npA->percent);
+}
+
 void man(){
 	printf("\n\tПособие по использованию программы:\n\tДля запуска используйте следующую структуру: ./start file1 file2 [-f]");
 	printf("\n\tДополнительные параметры: [-f] для сравнения file1 file2 и file2 file1 после чего выводится среднее значение.\n");
@@ -29,9 +37,9 @@ bool IsValidDirname(char* name){
 }
 
 //Return array of names and percent 
-np* Megafunction(char* filename, char* dirname, bool fullCompare, int *size){
+np* GetDirDiff(char* filename, char* dirname, bool fullCompare, size_t *size){
     static np res[MAXFILES]; 
-    int count = 0;
+    size_t count = 0;
 
     DIR *d;
     struct dirent *dir;
@@ -40,8 +48,10 @@ np* Megafunction(char* filename, char* dirname, bool fullCompare, int *size){
     {
         while ((dir = readdir(d)) != NULL)
         {
-            char* tfname = strcat(strcat(dirname, "/"), dir->d_name);
-            printf("%s\n", tfname);
+            if (strcmp(dir->d_name, ".")  == 0 || strcmp(dir->d_name, "..") == 0)
+                continue;
+            
+            char* tfname = strcat(strcat(strdup(dirname), "/"), dir->d_name);
             res[count].name1 = strdup(tfname);
             res[count].percent = CompareFiles(filename, tfname, fullCompare);
             count++;
@@ -61,7 +71,7 @@ void ParseArgs(int argc, char* argv[]){
         printf("\tНеверный ввод!\n");
         man();
     }
-    else if (argc >= 3 && IsValidFilename(argv[1]) && IsValidFilename(argv[2])){
+    else if (false && argc >= 3 && IsValidFilename(argv[1]) && IsValidFilename(argv[2])){
         if (argc == 4 && strcmp(argv[3], "-f") == 0)
             PrintDiff(CompareFiles(argv[1], argv[2], true));
         else
@@ -69,20 +79,20 @@ void ParseArgs(int argc, char* argv[]){
     }
     else if (argc >= 3 && IsValidFilename(argv[1]) && IsValidDirname(argv[2])){
         np* res;
-        int size;
+        size_t size;
         if (argc == 4 && strcmp(argv[3], "-f") == 0)
-            res = Megafunction(argv[1],argv[2], true, &size);
-        else   
-            res = Megafunction(argv[1],argv[2], false, &size);
+            res = GetDirDiff(argv[1],argv[2], true, &size);
+        else
+            res = GetDirDiff(argv[1],argv[2], false, &size);
+
+        qsort(res, size, sizeof(np), compare);
 
         for (size_t i = 0; i < size; i++)
         {
-            printf("n: %s p: %f \n", res[i].name1, res[i].percent);
+            printf("n: %s p: %f\n", res[i].name1, res[i].percent);
         }
         
     }
-
-
 }
 
 int main(int argc, char* argv[]){
